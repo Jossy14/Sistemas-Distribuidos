7ad35180a48438aaf76ed10a33235fe4bd3f87b3
Jossy14 <josiascontrerasm@gmail.com>
2024-09-06 23:02:43 -0500

    Guias

---
+++ ServidorTcpImc/src/josiascontreras/imc/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package josiascontreras.imc;
+
+import josiascontreras.imc.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author Josias
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
